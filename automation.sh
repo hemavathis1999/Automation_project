8dfb26ac7fb2895727958d2fda91d8222fb2fc5a
root <root@ip-172-31-85-3.ec2.internal>
2022-09-29 18:04:51 +0000

    2nd commit

--- automation.sh
+++ automation.sh
@@ -20,8 +20,54 @@ fi
 
 timestamp=$(date '+%d%m%Y-%H%M%S')
 cd /var/log/apache2
-tar -cf /tmp/${hema}-httpd-logs-${timestamp}.tar *.log
+tar -cf /tmp/$name-httpd-logs-${timestamp}.tar *.log
 
 aws s3 \
-cp /tmp/${hema}-httpd-logs-${timestamp}.tar \
+cp /tmp/$name-httpd-logs-${timestamp}.tar \
 s3://${s3_bucket}/${hema}-httpd-logs-${timestamp}.tar
+
+if [ -f "/var/www/html/inventory.html" ]
+then
+echo "File is found"
+else
+
+        echo "File creating ..........."
+	cat >>/var/www/html/inventory.html << EOF
+<table width="20%" cellspacing="0" cellpadding="5">
+        <thead>
+            <h4><th>Log Type </th></h4>
+            <h4><th>Time Created </th></h4>
+            <h4><th>Type </th></h4>
+            <h4><th>Size </th><h4>
+        </thead>
+</table>
+EOF
+fi
+
+size=$(du -h /tmp/$name-httpd-logs-${timestamp}.tar | awk '{print $1}')
+cat >>/var/www/html/inventory.html << EOF
+<table width="20%" cellspacing="0" cellpadding="5">
+        <tbody>
+       <tr>
+       <td>httpd-logs</td>
+       <td>${timestamp}</td>
+       <td>tar</td>
+       <td>$size</td>
+       </tr>
+       </tbody>
+</table>
+EOF
+
+
+
+if [ -f "/etc/cron.d/automation" ]
+then
+echo "File is found"
+
+else
+  cat >>/etc/cron.d/automation << EOF
+   0 12 * * *  /root/Automation_project/automation.sh
+EOF
+fi
+
+
