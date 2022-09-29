94aa68af43fba41d8bc0014f3588bc62015edcce
root <root@ip-172-31-85-3.ec2.internal>
2022-09-29 17:36:40 +0000

    1st commit

---
+++ automation.sh
@@ -0,0 +1,27 @@
+name="hema"
+s3_bucket="hss3"
+
+apt update -y
+
+if [[ apache2 != $(dpkg --get-selections apache 2 | awk '{print $1}' ) ]]; then
+        apt install apache2 -y
+fi
+
+running=$(systemctl status apache2 | grep active | awk '{print $3}' | tr -d '()' )
+if [[ running != ${running} ]]; then
+        systemctl start apache2
+
+fi
+
+enabled=$(systemctl is-enabled apache2 | grep "enabled" )
+if [[ enabled != ${enabled} ]]; then
+        systemctl enable apache2
+fi
+
+timestamp=$(date '+%d%m%Y-%H%M%S')
+cd /var/log/apache2
+tar -cf /tmp/${hema}-httpd-logs-${timestamp}.tar *.log
+
+aws s3 \
+cp /tmp/${hema}-httpd-logs-${timestamp}.tar \
+s3://${s3_bucket}/${hema}-httpd-logs-${timestamp}.tar
